84073981397ffa25726f7df966961ac5e8ac6f9d
KootBecker <u20426471@tuks.co.za>
2024-08-06 06:57:30 +0000

    Add WebServers

--- WebAppFirewall/web_app_firewall.py
+++ WebAppFirewall/web_app_firewall.py
@@ -1,12 +1,13 @@
-from flask import Flask,request,redirect,Response
+from flask import Flask,request,redirect,Response,session
 import requests, jwt
 import re
 import base64
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+import datetime
 app = Flask(__name__)
-SITE_NAME = 'http://127.0.0.1:8080/'
-session_requests = requests.session()
+app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+SITE_NAME = 'http://127.0.0.1:8001/'
 
 @app.before_request
 def before_request():
@@ -15,33 +16,53 @@ def before_request():
     # You can use the request.headers, request.method, request.path, etc. to make decisions
 
     # Example: Block requests from a specific IP address
-    if request.remote_addr == '127.0.0.1':
-        logger(f'Blocked request from IP address: {request.remote_addr}')
-        return 'Access denied', 403
+    # if request.remote_addr == '127.0.0.1':
+    #     logger(f'Blocked request from IP address: {request.remote_addr}')
+    #     return 'Access denied', 403
 
     # Example: Block requests to a specific path
-    if request.path == '/private':
-        logger(f'Blocked request to path: {request.path} from IP address: {request.remote_addr}')
-        return 'Access denied', 403
+    # if request.path == '/private':
+    #     logger(f'Blocked request to path: {request.path} from IP address: {request.remote_addr}')
+    #     return 'Access denied', 403
 
     # Example: Block requests with a specific user agent
-    if 'User-Agent' in request.headers and 'bad_agent' in request.headers['User-Agent']:
-        logger(f'Blocked request with bad user agent: {request.headers["User-Agent"]} from IP address: {request.remote_addr}')
-        return 'Access denied', 403
+    # if 'User-Agent' in request.headers and 'bad_agent' in request.headers['User-Agent']:
+    #     logger(f'Blocked request with bad user agent: {request.headers["User-Agent"]} from IP address: {request.remote_addr}')
+    #     return 'Access denied', 403
 
     # If none of the conditions match, allow the request to proceed
     return None
 
-@app.route('/<path:path>',methods=['GET','POST',"DELETE"])
-def proxy(path):
+@app.route('/',methods=['GET','POST',"DELETE"])
+def home():
     global SITE_NAME
-    global session_requests
     if request.method=='GET':
-        resp = session_requests.get(f'{SITE_NAME}{path}')
+        resp = requests.session().get(f'{SITE_NAME}')
         excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
         headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
         response = Response(resp.content, resp.status_code, headers)
-        if check_token(session_requests.cookies):
+        if check_token(requests.session().cookies):
+            return response
+        else:
+            logger(f'Blocked request to path: {request.path} from IP address: {request.remote_addr} due to invalid token')
+            return 'Access denied', 403
+
+    elif request.method=='POST':
+        resp = requests.post(f'{SITE_NAME}', data=request.form, headers=dict(request.headers))
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
+        response = Response(resp.content, resp.status_code, headers)
+        return response
+
+@app.route('/<path:path>',methods=['GET','POST',"DELETE"])
+def proxy(path):
+    global SITE_NAME
+    if request.method=='GET':
+        resp = requests.session().get(f'{SITE_NAME}{path}')
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
+        response = Response(resp.content, resp.status_code, headers)
+        if check_token(requests.session().cookies):
             return response
         else:
             logger(f'Blocked request to path: {request.path} from IP address: {request.remote_addr} due to invalid token')
@@ -50,10 +71,6 @@ def proxy(path):
         resp = requests.post(f'{SITE_NAME}{path}', data=request.form, headers=dict(request.headers))
         excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
         headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
-        return response
-    elif request.method=='DELETE':
-        resp = requests.delete(f'{SITE_NAME}{path}').content
         response = Response(resp.content, resp.status_code, headers)
         return response
 
@@ -75,10 +92,6 @@ def check_token(session_requests_cookies):
             if payload['role'] == 'admin':
                 return False
             return True
-
-def get_request_content(request):
-    # Write your logic to extract data from HTTP request
-    pass
 
 def check_signature_detection(session_requests_cookies):
     # Test for SQL Injection
@@ -141,7 +154,8 @@ def encode(jwt_string):
 
 def logger(message):
     with open('log.txt', 'a') as f:
-        f.write(message + '\n')
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        f.write(f'{current_time} - {message}\n')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug = False, port=5000)
+    app.run(host='0.0.0.0', debug = True, port=5000)
