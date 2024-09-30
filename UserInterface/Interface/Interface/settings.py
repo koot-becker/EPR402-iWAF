c5e03af7ffc748bc5b3897c01c72413d5cb381fc
KootBecker <u20426471@tuks.co.za>
2024-09-30 08:45:33 +0200

    Add Efficacy attributes to model

--- UserInterface/Interface/Interface/settings.py
+++ UserInterface/Interface/Interface/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xg*kko69&*6ht1mm)!n-k4@jbz=g7)=_7)k(p88o@4lanm8bt3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
--- UserInterface/Interface/waf_interface/views.py
+++ UserInterface/Interface/waf_interface/views.py
@@ -2,22 +2,34 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import WebApplication
 
-def web_applications(request):
+def main(request):
     web_applications = WebApplication.objects.all().values()
-    template = loader.get_template('web_applications.html')
+    template = loader.get_template('main.html')
     context = {
         'web_applications': web_applications
     }
     return HttpResponse(template.render(context, request))
 
 def web_app_1(request):
+    web_application = WebApplication.objects.all()[0]	
     template = loader.get_template('web_app_1.html')
-    return HttpResponse(template.render())
+    context = {
+        'web_application': web_application
+    }
+    return HttpResponse(template.render(context, request))
 
 def web_app_2(request):
+    web_application = WebApplication.objects.all()[0]	
     template = loader.get_template('web_app_2.html')
-    return HttpResponse(template.render())
+    context = {
+        'web_application': web_application
+    }
+    return HttpResponse(template.render(context, request))
 
 def web_app_3(request):
+    web_application = WebApplication.objects.all()[0]	
     template = loader.get_template('web_app_3.html')
-    return HttpResponse(template.render())
+    context = {
+        'web_application': web_application
+    }
+    return HttpResponse(template.render(context, request))
